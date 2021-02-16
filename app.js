9592bc396acd06c9ef0c4d91a9290129b7e585e1
Sharannyo Basu <yobasu2015@gmail.com>
2021-02-16 19:38:19 +0530

    Added navbar and Our Courses

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
-window.onscroll = function () {
-  myFunction();
-};
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > 650) {
+      $(".navig").css("background", "#0f0d63");
+    } else {
+      $(".navig").css("background", "none");
+    }
+  });
+});
